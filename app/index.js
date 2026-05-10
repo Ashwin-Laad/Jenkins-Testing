8307fd104efb944f38f9bd3fe80c8d43daaf44d5
Ashwin <ashwin95525@gmail.com>
2026-05-10 17:20:09 +0530

    fix app folder as normal directory

---
+++ app/index.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const app=express();
+const PORT=3000;
+app.get("/",(req,res,next)=>{
+  res.send("<h1>Hello from the express container</h1>");
+})
+
+app.use("/",(req,res,next)=>{
+  res.send("<h1>Go to the get page</h1>")
+})
+
+app.listen(PORT,()=>{
+  console.log("Server running on port"+PORT);
+})  
